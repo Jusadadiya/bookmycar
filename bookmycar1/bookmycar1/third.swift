0805be6c5ce8f167b8b6b6f454f5c90a70a8539d
Jay <usdadiyajay123@gmail.com>
2020-02-13 15:02:01 -0500

    age stepper fully working

--- bookmycar1/bookmycar1/third.swift
+++ bookmycar1/bookmycar1/third.swift
@@ -12,17 +12,23 @@ class third: UIViewController  ,UITextFieldDelegate {
 
     @IBOutlet weak var tf_name: UITextField!
    
+    @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var picker_gender: UIPickerView!
     var name:String = ""
-   
-    @IBAction func adder_age(_ sender: Any) {
+    
+    @IBOutlet weak var label_age: UILabel!
+    
+    var age:String=""
+    @IBAction func adder_age(_ sender: UIStepper) {
+        label_age.text = Int(sender.value).description
     }
    
     @IBAction func switch_insurance(_ sender: Any) {
     }
-    
+    var message:String=""
     @IBAction func submitBtnClicked(_ sender: Any) {
         name=tf_name.text!
+        age=label_age.text!
         if name==""
         {
         let alertController = UIAlertController(title: "iOScreator", message:
@@ -31,8 +37,9 @@ class third: UIViewController  ,UITextFieldDelegate {
 
         self.present(alertController, animated: true, completion: nil)
         }else {
+            message="Hello,\(name). We have received your information as follows. Your Age is \(age) years "
             let alertController = UIAlertController(title: "iOScreator", message:
-                name, preferredStyle: .alert)
+                message, preferredStyle: .alert)
             alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
 
             self.present(alertController, animated: true, completion: nil)
@@ -42,6 +49,11 @@ class third: UIViewController  ,UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        stepper.wraps = true
+        stepper.autorepeat = true
+        stepper.maximumValue = 100
+        stepper.minimumValue = 16
     }
 
 
