7eb40a4941ce09fd9264e9c58d86714c4837ac98
William <wreyesmartinez4361@conestogac.on.ca>
2020-02-12 22:12:14 -0500

    Adding pictures and description

--- bookmycar1/bookmycar1/FirstViewController.swift
+++ bookmycar1/bookmycar1/FirstViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-var lb_i1: UILabel!
+let pics = ["Ferrari F12", "porsche_taycan_turbo_2019", "Mercedes_A220", "Volvo_S90"]
+let maker = ["Ferrari", "Porsche", "Mercedes", "Volvo"]
+let model = ["F12", "Taycan", "A220", "S90"]
+
+var picscount = 0
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var tf_maker: UITextField!
+    @IBOutlet var tf_model: UITextField!
+    
     @IBOutlet var imgView: UIImageView!
+    
     @IBAction func btnChanges(_ sender: UIButton) {
-        imgView.image = UIImage(named: "Mercedez_Benz")
+        imgView.image = UIImage(named: pics[picscount])
+        tf_maker.text = maker[picscount]
+        tf_model.text = model[picscount]
+        picscount += 1
+        if picscount > 3{
+           picscount = 0
+        }
     }
     
     override func viewDidLoad() {
