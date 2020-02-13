6a6cab5e5bc58ad2d7053fd317088addbac5a980
Jay <usdadiyajay123@gmail.com>
2020-02-13 16:19:21 -0500

    finished 3rd page functionality

--- bookmycar1/bookmycar1/third.swift
+++ bookmycar1/bookmycar1/third.swift
@@ -20,12 +20,13 @@ class third: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
        @IBOutlet weak var label_age: UILabel!
        
        var age:String=""
+       var insurance:String=""
        @IBAction func adder_age(_ sender: UIStepper) {
            label_age.text = Int(sender.value).description
        }
       
-       @IBAction func switch_insurance(_ sender: Any) {
-       }
+    @IBOutlet weak var switch_insurance: UISwitch!
+    
        var message:String=""
     //number of columns of data
     
@@ -46,6 +47,12 @@ class third: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBAction func submitBtnClicked(_ sender: Any) {
         name=tf_name.text!
         age=label_age.text!
+        
+        if switch_insurance.isOn {
+            insurance = "with car"
+        } else {
+            insurance = "without car"
+        }
         if name==""
         {
         let alertController = UIAlertController(title: "iOScreator", message:
@@ -54,7 +61,7 @@ class third: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
         self.present(alertController, animated: true, completion: nil)
         }else {
-            message="Hello,\(name). We have received your information as follows. Your Age is \(age) years and gender \(gender) "
+            message="Hello,\(name). We have received your information as follows. Your Age is \(age) years and gender \(gender) and \(insurance) insurance "
             let alertController = UIAlertController(title: "iOScreator", message:
                 message, preferredStyle: .alert)
             alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
